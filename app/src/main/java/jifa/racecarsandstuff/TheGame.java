122f58027c92b9b6d2a82a6b581b0d27da90d1eb
YJoe <YJoe762@gmail.com>
2016-04-18 01:15:52 +0100

    Comments added fully for TheGame class

--- app/src/main/java/jifa/racecarsandstuff/TheGame.java
+++ app/src/main/java/jifa/racecarsandstuff/TheGame.java
@@ -8,21 +8,22 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class TheGame extends GameThread{
-    private View view;
     private Player player;
     private World world;
     private MainActivity activity;
     private Options options;
-    private Boolean notTriedYet;
+    private boolean notTriedYet;
 
     public TheGame(GameView gameView, MainActivity activity, Options options) {
+        // call the super constructor (from the GameThread class)
         super(gameView);
-        view = gameView;
         this.activity = activity;
         this.options = options;
         notTriedYet = true;
         setButtons(activity);
         int[][] points;
+        // switch the track flag passed to the game, tracks are constructed from a set of points
+        // by loading a different set of points a new track can be loaded
         switch(options.trackFlag){
             case 0: points = new int[][]{   {5, 15, 5}, {5, 5, 5}, {40, 5, 5}, {40, 14, 5},
                     {20, 14, 4}, {16, 18, 5} ,{16, 30, 5}, {29, 30, 5},
@@ -44,19 +45,26 @@ public class TheGame extends GameThread{
         // to divide the screen width by in order to get the correct scale for the phone.
         int scale = Math.round((float)activity.width / 120);
 
+        // create a world from a new track
         world = new World(new Track(gameView, points, scale, options), scale, mCanvasWidth, mCanvasHeight);
         int id = 0;
-        player = new Player(view, world, id, points, options);
+        // create the player
+        player = new Player(gameView, world, id, points, options);
+        // move all points to the center of the tracks
         for(int i = 0; i < points.length; i++){
             points[i][0] += points[i][2] /2;
             points[i][1] += points[i][2] /2;
         }
+        // if the options specifies ai should be present
         if(options.ai) {
+            // create 7 AI cars
             for (int i = 0; i < 7; i++) {
                 id++;
+                // set the start position to the coordinates of the start positions defined in the track
                 int x = world.track.startCoords.get(i).get(1) * world.scale * 10 + (10 * world.scale) - 10;
                 int y = world.track.startCoords.get(i).get(0) * world.scale * 10 + (10 * world.scale) + 50;
-                world.carList.add(new AICar(view, world, x, y, points, id, options));
+                // add the new ai car to the car list
+                world.carList.add(new AICar(gameView, world, x, y, points, id, options));
             }
         }
         player.world = world;
@@ -117,39 +125,42 @@ public class TheGame extends GameThread{
 
     @Override
     public void setupBeginning() {
+        // start the players lap timer
         player.startLapTimer();
+        // set the players position to the center of the screen
         player.xPos = mCanvasWidth/2;
         player.yPos = mCanvasHeight/2;
+        // set the world translate to the correct position
         world.setStartTranslate(mCanvasWidth, mCanvasHeight);
-        System.out.println(mCanvasWidth + " " + mCanvasHeight);
     }
 
     @Override
     protected void doDraw(Canvas canvas) {
+        // if the canvas is not null
         if (canvas != null) {
+            // draw the background
             super.doDraw(canvas);
+            // draw all world elements
             world.draw(canvas);
+            // draw the player
             player.draw(canvas);
         }
     }
 
     @Override
-    protected void actionOnTouch(float x, float y) {
-    }
-
-    @Override
-    protected void actionWhenPhoneMoved(float xDirection, float yDirection, float zDirection) {
-    }
-
-    @Override
     protected void updateGame(float secondsElapsed) {
+        // if the lapCount is equal to the defined lap count
         if(player.lapCount == options.lapCount && notTriedYet){
-            // not tried yet stops this update from happening twice
+            // not tried yet stops this update from happening twice and therefore
+            // wont submit high scores twice
             notTriedYet = false;
+            // run on the ui thread
             activity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+                    // pass all lap times in order to add them to the high scores file
                     activity.tryForHighScore(player.lapTimes, options.trackFlag);
+                    // display the single player stats screen
                     activity.singlePlayerStatsScreen(player.lapTimes, player.damage);
                 }
             });
